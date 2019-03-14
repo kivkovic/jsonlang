b118f023d76a11aad947b904651223910559d58b
Kresimir Ivkovic <kivkovic@pm.me>
2019-03-14 18:34:33 +0100

    Indexes blocks for displaying in error msgs

--- jsonlang.js
+++ jsonlang.js
@@ -1,17 +1,17 @@
 
-const juck = (input, functions = {}, vars = {}) =>
-    Array.isArray(input) ? input.map(line => exec(line, functions, vars)) :
-    typeof input === 'object' ? exec(input, functions, vars) :
+const juck = (input, functions = {}, vars = {}, lineNum = 0, callerLineNum = 0) =>
+    Array.isArray(input) ? input.map((line, i) => exec(line, functions, vars, lineNum + i + 1)) :
+    typeof input === 'object' ? exec(input, functions, vars, lineNum) :
     input;
 
-const exec = (block, functions, vars) => {
+const exec = (block, functions, vars, lineNum, callerLineNum) => {
 
     const keys = Object.keys(block),
         keylen = keys.length,
         value = keylen == 1 && block[keys[0]],
         operations = {
             /* not  */ '!'  : () => value.map ? value.map(e => !e) : !value,
-            /* if   */ '?'  : () => juck(block[juck(block['?'])|0], functions, vars),
+            /* if   */ '?'  : () => juck(block[juck(block['?'])|0], functions, vars, lineNum),
             /* prop */ '.'  : () => value.reduce((a, c) => a[juck(c, functions, vars)], juck(value[0], functions, vars)),
             /* eq   */ '==' : () => value.reduce((a, c, x, y, d = juck(c, functions, vars)) => (a == d) && d, value[0]),
             /* neq  */ '!=' : () => value.reduce((a, c, x, y, d = juck(c, functions, vars)) => (d == null || d != a) && d, null),
@@ -39,11 +39,11 @@ const exec = (block, functions, vars) => {
             params[argkey] = juck(arg, functions, vars);
 
             if (typeof params[argkey] != typeof fn.params[i][argkey]) {
-                throw `Line ${0}: Wrong argument type, expecting ${typeof fn.params[i][argkey]}, got ${typeof params[argkey]}`;
+                throw `Error in block ${lineNum}:\n${JSON.stringify(block)}\nWrong argument type for '${argkey}', expecting ${typeof fn.params[i][argkey]}, got ${typeof params[argkey]}`;
             }
         });
         const results = { ...fn.params, ...params };
-        const procedure = juck(fn.body, functions, results);
+        const procedure = juck(fn.body, functions, results, 0, lineNum);
         return procedure.length ? procedure[procedure.length - 1] : void 0;
     }
 
@@ -72,7 +72,7 @@ const exec = (block, functions, vars) => {
                 params: block[prop]['$'],
             }
         } else {
-            vars[prop] = juck(block[prop], functions, vars);
+            vars[prop] = juck(block[prop], functions, vars, lineNum);
         }
     }
 
